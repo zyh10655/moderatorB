12f433ccdefb460baa4ed4d3e7b0bfcb5b482dbb
NokiaZ <49866374+zyh10655@users.noreply.github.com>
2020-10-10 13:12:49 +1300

    Add files via upload

---
+++ build/precache-manifest.6b81ef76f9f708a21911bc4b06b877c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e0fac5cf07543b1a0ced35165366cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7ba9ff130284e24d268",
+    "url": "/static/css/main.2868838f.chunk.css"
+  },
+  {
+    "revision": "4e25b9a2686f3c8dc353",
+    "url": "/static/js/2.579b81ef.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.579b81ef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a7ba9ff130284e24d268",
+    "url": "/static/js/main.0a9e40a0.chunk.js"
+  },
+  {
+    "revision": "8dd7441d529e73d61d47",
+    "url": "/static/js/runtime-main.aaa24d6c.js"
+  }
+]);
